9da027e2cf1f6d13895d38d39f65f32d9dbfc64b
Tevin <tevinsopha@yahoo.com>
2022-08-02 09:42:51 +0700

    config firebase with flavor and add Kaopiz progresshud for global loading

--- android/app/src/main/kotlin/com/example/flutter_rawg/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_rawg/MainActivity.kt
@@ -1,6 +1,38 @@
 package com.example.flutter_rawg
 
 import io.flutter.embedding.android.FlutterActivity
+import com.kaopiz.kprogresshud.KProgressHUD
 
 class MainActivity: FlutterActivity() {
+
+    private val channel = "ChannelProgressHUD";
+    private val progressHUD: KProgressHUD? = null;
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
+            call, result ->
+            when (call.method){
+                "show" -> this.showLoading()
+                "hide" -> progressHUD?.dismiss()
+                else -> result.notImplemented()
+            }
+        }
+    }
+
+    private fun showLoading (): KProgressHUD? {
+        if(this.progressHUD != null){
+            if (this.progressHUD!!.isShowing) {
+                this.progressHUD!!.dismiss()
+            }
+        }
+        if(applicationContext != null){
+            progressHUD = KProgressHUD.create(this)
+                .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setCancellable(false)
+                .setDimAmount(0.5f);
+        }
+
+        return this.progressHUD?.show()
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rawg/utils/loading_modal.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,6 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    LoadingModal.show();
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
